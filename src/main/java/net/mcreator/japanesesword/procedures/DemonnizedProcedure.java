ec5cf54c646bca298acfc93b35364f04cd4f680c
hrmcngs <78349683+hrmcngs@users.noreply.github.com>
2022-10-28 19:55:47 +0900

    直した

--- src/main/java/net/mcreator/japanesesword/procedures/DemonnizedProcedure.java
+++ src/main/java/net/mcreator/japanesesword/procedures/DemonnizedProcedure.java
@@ -1,5 +1,10 @@
 package net.mcreator.japanesesword.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+
+import net.minecraft.world.World;
 import net.minecraft.item.ItemStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
@@ -9,24 +14,50 @@ import net.mcreator.japanesesword.enchantment.DemonizedEnchantment;
 import net.mcreator.japanesesword.JapaneseswordMod;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class DemonnizedProcedure {
+	@Mod.EventBusSubscriber
+	private static class GlobalTrigger {
+		@SubscribeEvent
+		public static void onEntityAttacked(LivingAttackEvent event) {
+			if (event != null && event.getEntity() != null) {
+				Entity entity = event.getEntity();
+				Entity sourceentity = event.getSource().getTrueSource();
+				Entity immediatesourceentity = event.getSource().getImmediateSource();
+				double i = entity.getPosX();
+				double j = entity.getPosY();
+				double k = entity.getPosZ();
+				double amount = event.getAmount();
+				World world = entity.world;
+				Map<String, Object> dependencies = new HashMap<>();
+				dependencies.put("x", i);
+				dependencies.put("y", j);
+				dependencies.put("z", k);
+				dependencies.put("amount", amount);
+				dependencies.put("world", world);
+				dependencies.put("entity", entity);
+				dependencies.put("sourceentity", sourceentity);
+				dependencies.put("immediatesourceentity", immediatesourceentity);
+				dependencies.put("event", event);
+				executeProcedure(dependencies);
+			}
+		}
+	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
-		if (dependencies.get("sourceentity") == null) {
-			if (!dependencies.containsKey("sourceentity"))
-				JapaneseswordMod.LOGGER.warn("Failed to load dependency sourceentity for procedure Demonnized!");
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				JapaneseswordMod.LOGGER.warn("Failed to load dependency entity for procedure Demonnized!");
 			return;
 		}
-		Entity sourceentity = (Entity) dependencies.get("sourceentity");
+		Entity entity = (Entity) dependencies.get("entity");
 		if ((EnchantmentHelper.getEnchantmentLevel(DemonizedEnchantment.enchantment,
-				((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)) != 0)) {
-			if (sourceentity instanceof LivingEntity)
-				((LivingEntity) sourceentity).setHealth((float) (((sourceentity instanceof LivingEntity)
-						? ((LivingEntity) sourceentity).getHealth()
-						: -1)
+				((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)) != 0)) {
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).setHealth((float) (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHealth() : -1)
 						+ EnchantmentHelper.getEnchantmentLevel(DemonizedEnchantment.enchantment,
-								((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY))));
+								((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))));
 		}
 	}
 }
