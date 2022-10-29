2479df95a78d550b670224d4ce213a9a58cee763
hrmcngs <78349683+hrmcngs@users.noreply.github.com>
2022-10-29 10:44:29 +0900

    直した

---
+++ src/main/java/net/mcreator/japanesesword/procedures/Demonized1Procedure.java
@@ -0,0 +1,35 @@
+package net.mcreator.japanesesword.procedures;
+
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.enchantment.EnchantmentHelper;
+
+import net.mcreator.japanesesword.potion.Arrow1PotionEffect;
+import net.mcreator.japanesesword.enchantment.DemonizedEnchantment;
+import net.mcreator.japanesesword.JapaneseswordMod;
+
+import java.util.Map;
+
+public class Demonized1Procedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				JapaneseswordMod.LOGGER.warn("Failed to load dependency entity for procedure Demonized1!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if ((EnchantmentHelper.getEnchantmentLevel(DemonizedEnchantment.enchantment,
+				((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)) != 0)) {
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Arrow1PotionEffect.potion, (int) 1, (int) 1, (true), (false)));
+		}
+		if ((EnchantmentHelper.getEnchantmentLevel(DemonizedEnchantment.enchantment,
+				((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemOffhand() : ItemStack.EMPTY)) != 0)) {
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Arrow1PotionEffect.potion, (int) 1, (int) 1, (true), (false)));
+		}
+	}
+}
