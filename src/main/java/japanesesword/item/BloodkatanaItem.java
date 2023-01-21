8388c365021ff51564db8decf5fa39109e6639ad
hrmcngs <78349683+hrmcngs@users.noreply.github.com>
2023-01-22 08:53:23 +0900

    直した

---
+++ src/main/java/japanesesword/item/BloodkatanaItem.java
@@ -0,0 +1,52 @@
+
+package japanesesword.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import japanesesword.JapaneseswordModElements;
+
+@JapaneseswordModElements.ModElement.Tag
+public class BloodkatanaItem extends JapaneseswordModElements.ModElement {
+	@ObjectHolder("japanesesword:bloodkatana")
+	public static final Item block = null;
+
+	public BloodkatanaItem(JapaneseswordModElements instance) {
+		super(instance, 388);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("bloodkatana"));
+	}
+}
